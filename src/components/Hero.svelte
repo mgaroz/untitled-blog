fdab1f57f43880b99d51ffdd4dd868aa160ef597
Miguel Garoz <mgaroz@gmail.com>
2023-04-11 04:52:38 -0600

    changed input width

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,15 +2,18 @@
   <div id="left-side">
     <h1 class="text-6xl font-semibold">Untitled Blog</h1>
     <div id="form" class="flex mt-4 w-[400px]">
-      <form action="#" class="w-full">
+      <form
+        action="#"
+        class="w-full focus:outline-transparent focus:ring-transparent focus:border-transparent"
+      >
         <input
           type="email"
           placeholder="Enter your email"
-          class="rounded-full py-3 px-4 border-r-0 border border-gray-400 rounded-e-none w-[65%] focus:outline-transparent focus:ring-transparent focus:border-transparent"
+          class="rounded-full py-3 px-4 border-r-0 border border-gray-400 rounded-e-none w-[63%] focus:outline-transparent focus:ring-transparent focus:border-transparent"
         />
         <button
           class="bg-black text-white rounded-full py-3 px-8 -translate-x-8 border border-gray-400 border-l-0"
-          type="submit">Subscribe</button
+          type="submit"><span class="font-bold">Subscribe</span></button
         >
       </form>
     </div>
